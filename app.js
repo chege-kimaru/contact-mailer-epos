e5dc01344332ef015c2074ff0c668696d498d8d2
chege-kimaru <chege-kimaru@gmail.com>
2021-03-24 17:37:19 +0300

    feat: customised for epos

--- app.js
+++ app.js
@@ -15,33 +15,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/sample", async (req, res) => {
+app.post("/contact", async (req, res) => {
   try {
     const data = await sendMail({
-      template: "sample",
-      subject: "Sample email",
-      context: { title: "Sample email" },
-    });
-    res.json({ data });
-  } catch (e) {
-    console.error(e);
-    res.status(500).json({ error: e.message });
-  }
-});
-
-app.post("/book", async (req, res) => {
-  try {
-    const data = await sendMail({
-      template: "book",
-      subject: "New Photoshoot Booking",
+      template: "contact",
+      subject: "New Message from Website Contact Form",
       context: {
-        title: "New Photoshoot Booking",
         name: req.body.name,
         email: req.body.email,
         phone: req.body.phone,
-        subject: req.body.subject,
-        date: req.body.date,
-        service: req.body.service,
+        // subject: req.body.subject,
+        date: new Date().toISOString(),
         message: req.body.message,
       },
     });
